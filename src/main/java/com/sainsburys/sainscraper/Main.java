7bbd463439fcb86817b46d5c1d0bd0865eba1e45
Dejan Lekic <dejan@lekic.org>
2016-02-07 20:34:17 +0000

    Some cleanup...

--- src/main/java/com/sainsburys/sainscraper/Main.java
+++ src/main/java/com/sainsburys/sainscraper/Main.java
@@ -10,16 +10,17 @@ import java.util.Arrays;
  */
 public class Main {
     public static final void main(String[] args) {
-        System.out.println(Arrays.toString(args));
         String urlStr = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html";
+        
         if (args.length == 1) {
             // If user gives URL as an argument to the application, we use it.
             urlStr = args[0];
         }
-        URL url;
+        
         try {
-            url = new URL(urlStr);
-            System.out.println(url);
+            URL url = new URL(urlStr);
+            Sainscraper scraper = new Sainscraper(url);
+            System.out.println(scraper.scrape());
         } catch (MalformedURLException ex) {
             System.out.println("The given web-address '" + urlStr + "' is not valid. Exiting....");
         }
